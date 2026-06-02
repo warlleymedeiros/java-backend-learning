05d547ef1e60a74d370df8dc167440cdf439c6bb
Warlley <wmedeiros.d@gmail.com>
2026-06-02 19:35:46 -0300

    Classes Vagas e Candidato

--- projeto_matchvagas/model/Candidato.java
+++ projeto_matchvagas/model/Candidato.java
@@ -3,7 +3,26 @@ package projeto_matchvagas.model;
 public record Candidato(
         String nome,
         String idade,
+        String numConta,
         String AreaTrabalho,
+        String Descricao,
         double ExpectativaSalario,
         boolean PrefereRemoto) {
+
+    @Override
+    public String toString() {
+
+        StringBuilder s = new StringBuilder();
+
+        s.append("Nome: ").append(nome).append("\n");
+        s.append("Idade: ").append(idade).append("\n");
+        s.append("Numero da Conta: ").append(numConta).append("\n");
+        s.append("Area de Atuação: ").append(AreaTrabalho).append("\n");
+        s.append("Expectativa de Salario: ").append(ExpectativaSalario).append("\n");
+        s.append("Prefere Remoto: ").append(PrefereRemoto).append("\n");
+        s.append("Descrição: ").append(Descricao).append("\n");
+        s.append("---------------\n");
+        return s.toString();
+
+    }
 }
--- projeto_matchvagas/model/Vagas.java
+++ projeto_matchvagas/model/Vagas.java
@@ -4,7 +4,21 @@ public record Vagas(
         String id,
         String titulo,
         String AreaAtuacao,
-        String salarioMaximo,
-        boolean aceitaRemoto
+        Double salarioMaximo,
+        boolean aceitaRemoto,
+        String Descricao
+
 ) {
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append("ID: ").append(id).append("\n");
+        s.append("Titulo: ").append(titulo).append("\n");
+        s.append("Area de Trabalho").append(AreaAtuacao).append("\n");
+        s.append("Salario Maximo: ").append(salarioMaximo).append("\n");
+        s.append("Pode trabalhar remoto: ").append(aceitaRemoto).append("\n");
+        s.append("Descrição: ").append(Descricao).append("\n");
+        s.append("---------------\n");
+        return s.toString();
+    }
 }

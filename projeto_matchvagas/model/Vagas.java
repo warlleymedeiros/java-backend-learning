2f9ff7c7015e17e9056c09dde7ad55741d6c7fcd
Warlley <warlleydiasxz@gmail.com>
2026-08-18 13:22:51 -0300

    arrumado o to String que estava imprimdo informações coladas

--- projeto_matchvagas/model/Vagas.java
+++ projeto_matchvagas/model/Vagas.java
@@ -14,7 +14,7 @@ public record Vagas(
         StringBuilder s = new StringBuilder();
         s.append("ID: ").append(id).append("\n");
         s.append("Titulo: ").append(titulo).append("\n");
-        s.append("Area de Trabalho").append(AreaAtuacao).append("\n");
+        s.append("Area de Trabalho: ").append(AreaAtuacao).append("\n");
         s.append("Salario Maximo: ").append(salarioMaximo).append("\n");
         s.append("Pode trabalhar remoto: ").append(aceitaRemoto).append("\n");
         s.append("Descrição: ").append(Descricao).append("\n");
